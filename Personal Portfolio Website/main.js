fe8dabc37898e01ab82b527f48ecbb3db57ecb6c
SIVA K <sivakathirvel2003@gmail.com>
2025-06-20 00:00:40 +0530

    Add files via upload

---
+++ Personal Portfolio Website/main.js
@@ -0,0 +1,7 @@
+var typedInstance = new Typed(".text", {
+    strings:["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
