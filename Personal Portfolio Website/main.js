956781f33285a85800912f4690f7b87edbbf26f9
SIVA K <sivakathirvel2003@gmail.com>
2025-06-21 00:17:17 +0530

    Delete Personal Portfolio Website directory

--- Personal Portfolio Website/main.js
+++
@@ -1,7 +0,0 @@
-var typedInstance = new Typed(".text", {
-    strings:["Frontend Developer", "Backend Developer", "Full Stack Developer"],
-    typeSpeed: 60,
-    backSpeed: 60,
-    backDelay: 1000,
-    loop: true
-});
